1d5ed034a4b8703a6daa71ff4322bc96f3c701d0
KrapivinAV <min_art@mail.ru>
2020-06-11 11:53:12 +0700

    Устраняет замечания Наставника к разделу 3-2

--- js/main.js
+++ js/main.js
@@ -6,19 +6,22 @@ var PIN_COORDINATE_Y_MIN = 130;
 var PIN_COORDINATE_Y_MAX = 630;
 var PRICE_MIN = 0;
 var PRICE_MAX = 100000;
-var CHECKIN_TIME_MIN = 12;
-var CHECKIN_TIME_MAX = 14;
+var CHECK_IN_OUT_TIME_MIN = 12;
+var CHECK_IN_OUT_TIME_MAX = 14;
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
 
 var map = document.querySelector('.map');
 var mapPinsField = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content;
 var pinButton = pinTemplate.querySelector('.map__pin');
-var pinImage = pinButton.querySelector('img');
+var fragment = document.createDocumentFragment();
 
 var types = ['palace', 'flat', 'house', 'bungalo'];
 var rooms = [1, 2, 3, 100];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var mapPinsFieldWidth = mapPinsField.clientWidth;
 
 var getRandomIntegerValue = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -54,51 +57,53 @@ var getSet = function (basisMassive) {
 var getMassiveDatabase = function () {
   var massiveDatabase = [];
   for (var i = 0; i < QUANTITY_OF_OFFERS; i++) {
-    var checkInOut = getRandomIntegerValue(CHECKIN_TIME_MIN, CHECKIN_TIME_MAX);
+    var ad = {}
 
-    massiveDatabase[i] = {};
+    ad.author = {};
+    ad.author.avatar = 'img/avatars/user0' + (i + 1) + '.png';
 
-    massiveDatabase[i].author = {};
-    massiveDatabase[i].author.avatar = 'img/avatars/user0' + (i + 1) + '.png';
+    ad.location = {};
+    ad.location.x = getRandomIntegerValue(PIN_COORDINATE_X_MIN, mapPinsFieldWidth);
+    ad.location.y = getRandomIntegerValue(PIN_COORDINATE_Y_MIN, PIN_COORDINATE_Y_MAX);
 
-    massiveDatabase[i].location = {};
-    massiveDatabase[i].location.x = getRandomIntegerValue(PIN_COORDINATE_X_MIN, mapPinsField.clientWidth);
-    massiveDatabase[i].location.y = getRandomIntegerValue(PIN_COORDINATE_Y_MIN, PIN_COORDINATE_Y_MAX);
+    ad.offer = {};
+    ad.offer.title = 'Заголовок предложения от Товарища №' + (i + 1);
+    ad.offer.address = '(' + ad.location.x + ',' + ad.location.y + ')';
+    ad.offer.price = getRandomIntegerValue(PRICE_MIN, PRICE_MAX);
+    ad.offer.type = types[getRandomIntegerValue(0, types.length - 1)];
+    ad.offer.rooms = rooms[getRandomIntegerValue(0, rooms.length - 1)];
+    ad.offer.guests = getGuestsQuantity(ad.offer.rooms);
+    ad.offer.checkin = getRandomIntegerValue(CHECK_IN_OUT_TIME_MIN, CHECK_IN_OUT_TIME_MAX); + ':00';
+    ad.offer.checkout = getRandomIntegerValue(CHECK_IN_OUT_TIME_MIN, CHECK_IN_OUT_TIME_MAX); + ':00';
+    ad.offer.features = getSet(features);
+    ad.offer.description = 'Описание предложения от Товарища №' + (i + 1);
+    ad.offer.photos = getSet(photos);
 
-    massiveDatabase[i].offer = {};
-    massiveDatabase[i].offer.title = 'Заголовок предложения от Товарища №' + (i + 1);
-    massiveDatabase[i].offer.address = '(' + massiveDatabase[i].location.x + ',' + massiveDatabase[i].location.y + ')';
-    massiveDatabase[i].offer.price = getRandomIntegerValue(PRICE_MIN, PRICE_MAX);
-    massiveDatabase[i].offer.type = types[getRandomIntegerValue(0, types.length - 1)];
-    massiveDatabase[i].offer.rooms = rooms[getRandomIntegerValue(0, rooms.length - 1)];
-    massiveDatabase[i].offer.guests = getGuestsQuantity(massiveDatabase[i].offer.rooms);
-    massiveDatabase[i].offer.checkin = 'C ' + checkInOut + ':00';
-    massiveDatabase[i].offer.checkout = 'До ' + checkInOut + ':00';
-    massiveDatabase[i].offer.features = getSet(features);
-    massiveDatabase[i].offer.description = 'Описание предложения от Товарища №' + (i + 1);
-    massiveDatabase[i].offer.photos = getSet(photos);
+    massiveDatabase.push(ad);
   }
   return massiveDatabase;
 };
 
 var createPin = function (data) {
   var pin = pinButton.cloneNode(true);
-  pin.style = 'left: ' + (data.location.x - 25) + 'px; top: ' + (data.location.y - 70) + 'px;';
+  var pinImage = pin.querySelector('img');
+  pin.style = 'left: ' + (data.location.x - PIN_WIDTH / 2) + 'px; top: ' + (data.location.y - PIN_HEIGHT) + 'px;';
   pinImage.src = data.author.avatar;
   pinImage.alt = data.offer.title;
   return pin;
 };
 
 var addPin = function (newPin) {
-  mapPinsField.appendChild(newPin);
+  fragment.appendChild(newPin);
 };
 
 var database = getMassiveDatabase();
-console.log(database);
-
-map.classList.remove('map--faded');
 
 for (var i = 0; i < database.length; i++) {
   var mapPin = createPin(database[i]);
   addPin(mapPin);
 }
+
+mapPinsField.appendChild(fragment);
+
+map.classList.remove('map--faded');
