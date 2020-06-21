19319e09f2d7f1cc5532534b03d594d161dc2472
KrapivinAV <min_art@mail.ru>
2020-06-21 20:55:01 +0700

    Добавляет карточки предложений дубль 4

--- js/main.js
+++ js/main.js
@@ -10,16 +10,37 @@ var CHECK_IN_OUT_TIME_MIN = 12;
 var CHECK_IN_OUT_TIME_MAX = 14;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
 var TRASLATED_TYPES = {
   palace: 'Дворец',
   flat: 'Квартира',
   house: 'Дом',
   bungalo: 'Бунгало'
 };
-var ROOMS_VARIANTS = [1, 2, 3, 100];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var ROOMS_VARIANTS = [
+  1,
+  2,
+  3,
+  100
+];
+var FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
 
 var map = document.querySelector('.map');
 var mapPinsField = document.querySelector('.map__pins');
@@ -30,7 +51,6 @@ var cardTemplate = document.querySelector('#card').content;
 var cardPopup = cardTemplate.querySelector('.popup');
 var pinFragment = document.createDocumentFragment();
 var cardFragment = document.createDocumentFragment();
-
 var mapPinsFieldWidth = mapPinsField.clientWidth;
 
 var getRandomIntegerValue = function (min, max) {
@@ -68,6 +88,8 @@ var getMassiveDatabase = function () {
   var massiveDatabase = [];
   for (var i = 0; i < QUANTITY_OF_OFFERS; i++) {
     var roomsSet = ROOMS_VARIANTS[getRandomIntegerValue(0, ROOMS_VARIANTS.length - 1)];
+    var xTemp = getRandomIntegerValue(PIN_COORDINATE_X_MIN, mapPinsFieldWidth);
+    var yTemp = getRandomIntegerValue(PIN_COORDINATE_Y_MIN, PIN_COORDINATE_Y_MAX);
     var ad = {
 
       author: {
@@ -75,13 +97,13 @@ var getMassiveDatabase = function () {
       },
 
       location: {
-        x: getRandomIntegerValue(PIN_COORDINATE_X_MIN, mapPinsFieldWidth),
-        y: getRandomIntegerValue(PIN_COORDINATE_Y_MIN, PIN_COORDINATE_Y_MAX)
+        x: xTemp,
+        y: yTemp
       },
 
       offer: {
         title: 'Заголовок предложения от Товарища №' + (i + 1),
-        address: '(' + location.x + ',' + location.y + ')',
+        address: '(' + xTemp + ',' + yTemp + ')',
         price: getRandomIntegerValue(PRICE_MIN, PRICE_MAX),
         type: TYPES[getRandomIntegerValue(0, TYPES.length - 1)],
         rooms: roomsSet,
@@ -135,14 +157,20 @@ var createCard = function (data) {
   cardTime.textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
 
   for (var i = FEATURES.length - 1; i >= 0; i--) {
-    data.offer.features.includes(FEATURES[i]) ? cardFeaturesItems[i].textContent = FEATURES[i] : cardFeatures.removeChild(cardFeaturesItems[i]);
+    if (data.offer.features.includes(FEATURES[i])) {
+      cardFeaturesItems[i].textContent = FEATURES[i];
+    } else {
+      cardFeatures.removeChild(cardFeaturesItems[i]);
+    }
   }
 
   cardDescription.textContent = data.offer.description;
 
-  for (var j = 0; j < data.offer.photos.length; j++) {
-    cardPhoto.src = data.offer.photos[j];
-    j !== (data.offer.photos.length - 1) ? cardPhotos.appendChild(cardPhoto.cloneNode()) : null;
+  for (i = 0; i < data.offer.photos.length; i++) {
+    cardPhoto.src = data.offer.photos[i];
+    if (i !== (data.offer.photos.length - 1)) {
+      cardPhotos.appendChild(cardPhoto.cloneNode());
+    }
   }
 
   return card;
