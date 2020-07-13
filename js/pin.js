a48f7d9e8b1315dce7f79908c5492dcca52d0109
KrapivinAV <min_art@mail.ru>
2020-07-13 11:08:19 +0700

    Устраняет проблему слияния 2

--- js/pin.js
+++ js/pin.js
@@ -3,11 +3,7 @@
 window.pin = (function () {
 
   return {
-<<<<<<< HEAD
-    create: function(database) {
-=======
     create: function (database) {
->>>>>>> 9dc7e1b1840e64a1aef13cc716b109e888987194
       var pinTemplate = document.querySelector('#pin').content;
       var pinButton = pinTemplate.querySelector('.map__pin');
       var pinFragment = document.createDocumentFragment();
@@ -25,16 +21,6 @@ window.pin = (function () {
         pinFragment.appendChild(newPin);
       };
 
-<<<<<<< HEAD
-      for (var i = 0; i < database.length; i++) {
-        var mapPin = createPin(database[i]);
-        addPin(mapPin);
-      }
-
-      return pinFragment;
-    }
-  }
-=======
       database.forEach(function (item) {
         var mapPin = createPin(item);
         addPin(mapPin);
@@ -43,5 +29,4 @@ window.pin = (function () {
       return pinFragment;
     }
   };
->>>>>>> 9dc7e1b1840e64a1aef13cc716b109e888987194
 })();
